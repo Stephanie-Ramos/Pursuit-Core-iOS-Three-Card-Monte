03551d544a18df0d13d077330a1d39eaea074dad
Stephanie Ramos <ramos.n.stephanie@outlook.com>
2019-11-01 07:55:24 -0400

    updated file

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -12,11 +12,37 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
   }
-
-    @IBAction func card1(_ sender: UIButton) {
-    }
     
-}
+    @IBAction func card1(_ sender: UIButton){}
+    @IBAction func card2(_ sender: UIButton){}
+    @IBAction func card3(_ sender: UIButton){}
+    
+        enum Cards: String {
+            case threeCard, kingCard, cardBackRed
+        }
 
+        class ThreeCardMonte {
+        var cards: [Cards] = [.cardBackRed,.kingCard,.threeCard]
+        var cardsFlipped = false
+        var card1: Cards
+        var card2: Cards
+        var card3: Cards
+            
+            init() {
+                cards = cards.shuffled()
+                card1 = cards[0]
+                card2 = cards[1]
+                card3 = cards[2]
+            }
+       func cardShuffle() {
+           cards = cards.shuffled()
+           card1 = cards[0]
+           card2 = cards[1]
+           card3 = cards[2]
+       }
+    }
+}
+        
+// show another image
+// lose, shows card and the card
